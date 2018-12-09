5207e22a8d6e0c90a401591a20e36e62801e87a2
asd20643 <42505230+asd20643@users.noreply.github.com>
2018-12-09 09:31:01 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -22,6 +22,49 @@ $(document).ready(function() {
 		}
 	})
 
+	// font color of navbar change when scroll
+	$(window).on('scroll',function(){
+		var scrollDistance = $(window).scrollTop();
+		var sceenHeight = $(window).height();
+		var $header = $(".js-header");
+		if(scrollDistance < sceenHeight){
+			$header.addClass("active__firstPage");
+		}
+		else{
+			$header.removeClass("active__firstPage")
+		}
+		if((scrollDistance >= (sceenHeight * 1)) && (scrollDistance < (sceenHeight * 2))){
+			$header.addClass("active__secondPage");
+		}
+		else{
+			$header.removeClass("active__secondPage")
+		}
+		if((scrollDistance >= (sceenHeight * 2)) && (scrollDistance < (sceenHeight * 3))){
+			$header.addClass("active__thirdPage");
+		}
+		else{
+			$header.removeClass("active__thirdPage")
+		}
+		if((scrollDistance >= (sceenHeight * 3)) && (scrollDistance < (sceenHeight * 4))){
+			$header.addClass("active__4thPage");
+		}
+		else{
+			$header.removeClass("active__4thPage")
+		}
+		if((scrollDistance >= (sceenHeight * 4)) && (scrollDistance < (sceenHeight * 5))){
+			$header.addClass("active__5thPage");
+		}
+		else{
+			$header.removeClass("active__5thPage")
+		}
+		if((scrollDistance >= (sceenHeight * 5)) && (scrollDistance < (sceenHeight * 6))){
+			$header.addClass("active__6thPage");
+		}
+		else{
+			$header.removeClass("active__6thPage")
+		}
+	})
+
 	// rellax initialization
 	if(screen.width > 768){
 		var rellax = new Rellax('.rellax',{
